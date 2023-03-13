326729176ff6e4fb8e8ad8c2e81b2e3a71fbf6ba
ChrisOnMac <20260809+schmizzel@users.noreply.github.com>
2023-03-13 14:05:23 +0100

    added open box script

---
+++ Assets/Scripts/OpenBox.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class OpenBox : MonoBehaviour
+{
+    void Start() {}
+
+    // Update is called once per frame
+    void Update(){}
+
+    public void openRemoteBox() {
+        Debug.Log("opening box");
+        StartCoroutine("http://10.158.41.69:3000/api/open");
+    }
+
+    IEnumerator getRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for result
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
+}
